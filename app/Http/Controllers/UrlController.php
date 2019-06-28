3f411827896a0f56f696f8d4176d744f56a12cca
Stanislau Piuchanka <Vladimir@sweetdefeat.com>
2019-06-28 19:34:28 +0300

    travis

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -9,18 +9,20 @@ use function getUrlIdentifier;
 
 class UrlController extends Controller
 {
-    public function getShortUrl(Request $request) {
+    public function getShortUrl(Request $request)
+    {
         return response()->json(['success' => true, 'data' => [getShortUrlById(12345), getUrlIdentifier('dnh')]]);
     }
 
-    public function createUrl(Request $request) {
-        $link = Link::create([
+    public function createUrl(Request $request)
+    {
+        $link = Link::create(
+            [
             'original_url' => $request->input('original_url')
-        ]);
+            ]
+        );
         $link->code = getShortUrlById($link->id);
         $link->save();
         return response()->json(['success' => true, 'data' => Link::find($link->id)]);
     }
 }
-
-
--- makefile
+++ makefile
@@ -1,2 +1,6 @@
 test:
 	composer run test
+generate-key:
+	composer run post-create-project-cmd
+codesniffer:
+	composer run codesniffer
